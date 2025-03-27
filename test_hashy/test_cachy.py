7c3548cf20328db84464f6f021ed9bca8d01bc12
JamesAbel <j@abel.co>
2025-03-27 10:56:55 -0700

    cache to a SQLite DB add in-memory cache as an option

--- test_hashy/test_cachy.py
+++ test_hashy/test_cachy.py
@@ -2,60 +2,86 @@ import time
 from pathlib import Path
 from datetime import timedelta
 import os
-import shutil
 from pprint import pformat
 from typing import List
+import math
 
 from hashy import cachy
 from hashy.cache import get_cache_dir, CacheCounters, clear_counters, get_counters
 
 cache_life = timedelta(days=1)
 
-cache_directory = Path(os.environ.get("RUNNER_TEMP", "temp"))  # for GitHub actions
 
-
-def rm_cache_dir():
-    shutil.rmtree(cache_directory, ignore_errors=True)
+def get_cache_directory() -> Path:
+    cache_directory = Path(os.environ.get("RUNNER_TEMP", "temp"))  # for GitHub actions
+    return cache_directory
 
 
 def test_a_cachy_zero_life():
-    rm_cache_dir()
+
     clear_counters()
 
-    @cachy(timedelta(seconds=0), cache_directory)
+    @cachy(timedelta(seconds=0), get_cache_directory())
     def func(p):
         return p
 
     assert func(1) == 1
-    assert get_counters() == CacheCounters(cache_hit_counter=0, cache_miss_counter=1, cache_load_counter=0, cache_expired_counter=0)
+    assert get_counters() == CacheCounters(cache_memory_hit_counter=0, cache_hit_counter=0, cache_miss_counter=1, cache_expired_counter=0)
     time.sleep(0.1)  # cache will expire
     assert func(1) == 1
-    assert get_counters() == CacheCounters(cache_hit_counter=0, cache_miss_counter=2, cache_load_counter=0, cache_expired_counter=1)
+    assert get_counters() == CacheCounters(cache_memory_hit_counter=0, cache_hit_counter=0, cache_miss_counter=2, cache_expired_counter=1)
 
 
 def test_cachy_simple():
-    rm_cache_dir()
-    clear_counters()
 
-    @cachy(cache_life, cache_directory)
-    def func(p):
+    @cachy(cache_life, get_cache_directory())
+    def cachy_simple_func(p):
         return p
 
-    assert func(1) == 1
-    assert get_counters() == CacheCounters(cache_hit_counter=0, cache_miss_counter=1, cache_load_counter=0, cache_expired_counter=0)
-    assert func(1) == 1
-    assert get_counters() == CacheCounters(cache_hit_counter=1, cache_miss_counter=1, cache_load_counter=0, cache_expired_counter=0)
-    assert func(1) == 1
-    assert get_counters() == CacheCounters(cache_hit_counter=2, cache_miss_counter=1, cache_load_counter=0, cache_expired_counter=0)
-    assert func(2) == 2
-    assert get_counters() == CacheCounters(cache_hit_counter=2, cache_miss_counter=2, cache_load_counter=0, cache_expired_counter=0)
-    assert func(2) == 2
-    assert get_counters() == CacheCounters(cache_hit_counter=3, cache_miss_counter=2, cache_load_counter=0, cache_expired_counter=0)
-    assert pformat(get_counters()) == "cache_hit_counter=3,cache_miss_counter=2,cache_load_counter=0,cache_expired_counter=0"
+    clear_counters()
+    # delete the DB so the cache counts are correct
+    if (glob_results := list(get_cache_directory().glob("cachy_simple_func*"))) and len(glob_results) > 0:
+        glob_results[0].unlink(missing_ok=True)
+
+    assert cachy_simple_func(1) == 1
+    assert get_counters() == CacheCounters(cache_memory_hit_counter=0, cache_hit_counter=0, cache_miss_counter=1, cache_expired_counter=0)
+    assert cachy_simple_func(1) == 1
+    assert get_counters() == CacheCounters(cache_memory_hit_counter=0, cache_hit_counter=1, cache_miss_counter=1, cache_expired_counter=0)
+    assert cachy_simple_func(1) == 1
+    assert get_counters() == CacheCounters(cache_memory_hit_counter=0, cache_hit_counter=2, cache_miss_counter=1, cache_expired_counter=0)
+    assert cachy_simple_func(2) == 2
+    assert get_counters() == CacheCounters(cache_memory_hit_counter=0, cache_hit_counter=2, cache_miss_counter=2, cache_expired_counter=0)
+    assert cachy_simple_func(2) == 2
+    assert get_counters() == CacheCounters(cache_memory_hit_counter=0, cache_hit_counter=3, cache_miss_counter=2, cache_expired_counter=0)
+    assert pformat(get_counters()) == "cache_memory_hit_counter=0,cache_hit_counter=3,cache_miss_counter=2,cache_expired_counter=0"
+
+
+def test_cachy_simple_in_memory():
+
+    @cachy(cache_life, get_cache_directory(), in_memory=True)
+    def cachy_simple_func(p):
+        return p
+
+    clear_counters()
+    # delete the DB so the cache counts are correct
+    if (glob_results := list(get_cache_directory().glob("cachy_simple_func*"))) and len(glob_results) > 0:
+        glob_results[0].unlink(missing_ok=True)
+
+    assert cachy_simple_func(1) == 1
+    assert get_counters() == CacheCounters(cache_memory_hit_counter=0, cache_hit_counter=0, cache_miss_counter=1, cache_expired_counter=0)
+    assert cachy_simple_func(1) == 1
+    assert get_counters() == CacheCounters(cache_memory_hit_counter=1, cache_hit_counter=1, cache_miss_counter=1, cache_expired_counter=0)
+    assert cachy_simple_func(1) == 1
+    assert get_counters() == CacheCounters(cache_memory_hit_counter=2, cache_hit_counter=2, cache_miss_counter=1, cache_expired_counter=0)
+    assert cachy_simple_func(2) == 2
+    assert get_counters() == CacheCounters(cache_memory_hit_counter=2, cache_hit_counter=2, cache_miss_counter=2, cache_expired_counter=0)
+    assert cachy_simple_func(2) == 2
+    assert get_counters() == CacheCounters(cache_memory_hit_counter=3, cache_hit_counter=3, cache_miss_counter=2, cache_expired_counter=0)
+    assert pformat(get_counters()) == "cache_memory_hit_counter=3,cache_hit_counter=3,cache_miss_counter=2,cache_expired_counter=0"
 
 
 def test_cachy_dict():
-    @cachy(cache_life, cache_directory)
+    @cachy(cache_life, get_cache_directory())
     def func(p):
         return p
 
@@ -64,16 +90,15 @@ def test_cachy_dict():
     assert func(data) == data
 
 
-def test_cachy_performance():
+def test_cachy_performance_small_data():
     # test that cachy is actually caching
 
-    rm_cache_dir()
     clear_counters()
 
-    short_time = 0.01
+    short_time = 0.02
     long_time = 0.1
 
-    @cachy(cache_life, cache_directory)
+    @cachy(cache_life, get_cache_directory())
     def takes_a_long_time(p):
         time.sleep(1.01 * long_time)
         return p
@@ -93,8 +118,58 @@ def test_cachy_performance():
     assert duration < short_time
 
 
+def test_cachy_performance_time_consuming_data_calculation():
+
+    clear_counters()
+
+    # cache in DB (not in memory)
+    @cachy(cache_life, get_cache_directory())
+    def time_consuming_data_calculation(n):
+        result = sum([math.sin(i) for i in range(int(n))])
+        return result
+
+    start = time.time()
+    big_n = int(1e6)
+    time_consuming_data_calculation(big_n)
+    cold_duration = 1000.0 * (time.time() - start)  # mS
+
+    start = time.time()
+    time_consuming_data_calculation(big_n)
+    warm_duration = 1000.0 * (time.time() - start)  # mS
+
+    speedup = cold_duration / warm_duration
+    print(f"{cold_duration=:.2f} mS,{warm_duration=:.2f} mS,{speedup=:.2f}x")
+
+    assert speedup > 10  # 30x has been observed
+
+
+def test_cachy_performance_time_consuming_data_calculation_in_memory():
+
+    clear_counters()
+
+    # cache in memory
+    @cachy(cache_life, get_cache_directory(), in_memory=True)
+    def time_consuming_data_calculation_in_memory(n):
+        result = sum([math.sin(i) for i in range(int(n))])
+        return result
+
+    start = time.time()
+    big_n = int(1e6)
+    time_consuming_data_calculation_in_memory(big_n)
+    cold_duration = 1000.0 * (time.time() - start)  # mS
+
+    start = time.time()
+    time_consuming_data_calculation_in_memory(big_n)
+    warm_duration = 1000.0 * (time.time() - start)  # mS
+
+    speedup = cold_duration / warm_duration
+    print(f"{cold_duration=:.2f} mS,{warm_duration=:.2f} mS,{speedup=:.2f}x")
+
+    assert speedup > 100  # 800x has been observed
+
+
 def test_cachy_complex():
-    @cachy(cache_life, cache_directory)
+    @cachy(cache_life, get_cache_directory())
     def func(a, b, c):
         return [a, b, c]
 
@@ -126,10 +201,9 @@ def test_cachy_dir_default_slow():
 
 def test_cachy_all_defaults():
 
-    rm_cache_dir()
     clear_counters()
 
-    @cachy(cache_dir=cache_directory)
+    @cachy(cache_dir=get_cache_directory())
     def func(a):
         time.sleep(0.1)
         return a + a
@@ -146,10 +220,9 @@ def test_cachy_all_defaults():
 
 def test_cachy_different_values():
 
-    rm_cache_dir()
     clear_counters()
 
-    @cachy(cache_dir=cache_directory)
+    @cachy(cache_dir=get_cache_directory())
     def func(a):
         time.sleep(0.1)
         return a + a
@@ -161,17 +234,16 @@ def test_cachy_different_values():
 
 def test_cachy_big_data():
 
-    @cachy(cache_life, cache_directory)
+    @cachy(cache_life, get_cache_directory())
     def big_to_little(a: List[int]) -> int:
         return len(a)
 
-    @cachy(cache_life, cache_directory)
+    @cachy(cache_life, get_cache_directory())
     def little_to_big(a: int) -> List[int]:
         return [v for v in range(a)]
 
-    rm_cache_dir()
     clear_counters()
-    for n in (100, 1000):
+    for n in (100, int(1e6)):
         data = [v for v in range(n)]
 
         # bools since if there's a mis-compare the diff will be printed which takes a really long time
@@ -181,4 +253,4 @@ def test_cachy_big_data():
         assert bool(little_to_big(n) == data)
         assert bool(big_to_little(data) == n)
 
-    assert get_counters() == CacheCounters(cache_hit_counter=6, cache_miss_counter=4, cache_load_counter=0, cache_expired_counter=0)
+    assert get_counters() == CacheCounters(cache_memory_hit_counter=0, cache_hit_counter=6, cache_miss_counter=4, cache_expired_counter=0)
